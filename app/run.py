8b19d59677613c6fe7e80e4d78525b23ac2dc77b
Masamune Utsunomiya <masamunet+github@gmail.com>
2024-11-21 11:04:48 +0900

    Update app.run() to bind to all network interfaces

--- app/run.py
+++ app/run.py
@@ -1,4 +1,4 @@
 from app import app
     #データベースの設定やテストデータの挿入に何か知らのコードを追加するなど、変更を加えたらdata.dbのファイルを削除して再度実行。
 if __name__ == '__main__':
-    app.run(debug=True, port=8888, threaded=True)  
+    app.run(debug=True, host="0.0.0.0", port=8888, threaded=True)  
