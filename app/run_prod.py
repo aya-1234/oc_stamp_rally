21818dd4b8f86faf9f0898825b3201efd073e7fa
Masamune Utsunomiya <masamunet+github@gmail.com>
2024-11-27 18:26:25 +0900

    Refactor run_prod.py to remove code for deleting existing data.db file

--- app/run_prod.py
+++ app/run_prod.py
@@ -11,6 +11,22 @@ logging.basicConfig(
 )
 
 if __name__ == '__main__':
+    
+    # import os
+    
+    # # data.dbファイルのパスを設定
+    # db_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "instance", "data.db")
+    
+    # # ファイルが存在する場合は削除
+    # if os.path.exists(db_path):
+    #     try:
+    #         os.remove(db_path)
+    #         logging.info("Existing data.db file was removed")
+    #     except Exception as e:
+    #         logging.error(f"Error removing data.db: {str(e)}")
+
+
+
     try:
         logging.info("Starting Flask application...")
         app.run(debug=False, host="0.0.0.0", port=8888, threaded=True)
